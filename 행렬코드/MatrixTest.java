799905e400ebd14135deb47df3d5c1361c4098f5
bepyan <rlantia97@kumoh.ac.kr>
2021-03-30 00:29:14 +0900

    week-1

---
+++ 행렬코드/MatrixTest.java
@@ -0,0 +1,11 @@
+
+public class MatrixTest {
+
+	public static void main(String[] args) {
+		Matrix mat = new Matrix(4,4);
+		mat.firstMat();
+		mat.left();
+		mat.right();
+		mat.transMat();
+	}
+}
